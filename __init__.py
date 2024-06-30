5a883f475dd2c7598a396c8dafe9eb592687477b
Aimi14 <167098557+Aimi14@users.noreply.github.com>
2024-06-30 18:20:25 +0800

    app

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.routes import initialize_routes
 
-app = Flask(__name__)
-initialize_routes(app)
+apps = Flask(__name__)
+initialize_routes(apps)
 
 # Any other initial setup can go here
